ac4e33967dabe8dd2340a171c5a500bbb7904518
iTanvir <thisistanvir.dev@gmail.com>
2023-05-03 16:49:18 +0600

    Fixed Issues

--- wp-content/themes/halle47/assets/js/main.js
+++ wp-content/themes/halle47/assets/js/main.js
@@ -32,24 +32,4 @@
 		.mouseout(function () {
 			$(this).attr("src", $(this).data("src"));
 		});
-
-	// Smooth Scroll for IE/ EDGE/ SAFARI
-	$("a").on("click", function (event) {
-		if (this.hash !== "") {
-			event.preventDefault();
-
-			var hash = this.hash;
-
-			$("html, body").animate(
-				{
-					scrollTop: $(hash).offset().top,
-				},
-				800,
-				function () {
-					window.location.hash = hash;
-				}
-			);
-		}
-	});
-	// End Smooth Scroll for IE/ EDGE/ SAFARI
 })(jQuery);
